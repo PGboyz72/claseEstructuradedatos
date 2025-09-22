a0e1b5f06d78f4f426a965610d2d446cd61d11d3
andrick alain preciado gonzalez <andrickprego@gmail.com>
2025-09-21 19:16:01 -0500

    TALLER 1 N1

--- TALLER 1 N1/taller.c
+++ TALLER 1 N1/taller.c
@@ -1,89 +1,89 @@
 #include <stdio.h>
-
 #define MAX 10
 
-
-void selectionSort(int arr[], int n);
-void insertionSort(int arr[], int n);
-void imprimirArreglo(int arr[], int n);
+void llenarArray(int vector[MAX], int n);
+void mostrarArray(int vector[MAX], int n);
+void selectionSort(int array[MAX], int n);
+void insertionSort(int arr[MAX], int n);
 
 int main() {
-    int arreglo[MAX], n, opcion;
-
-    printf("Ingrese el numero de elementos (max %d): ", MAX);
-    scanf("%d", &n);
-
-    if (n > MAX || n <= 0) {
-        printf("Cantidad invalida.\n");
-        return 1;
-    }
-
-    printf("Ingrese los elementos del arreglo:\n");
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arreglo[i]);
-    }
-
-    printf("\nElija el metodo de ordenamiento:\n");
-    printf("1. Selection Sort\n");
-    printf("2. Insertion Sort\n");
-    printf("Opcion: ");
-    scanf("%d", &opcion);
-
-    switch (opcion) {
-        case 1:
-            selectionSort(arreglo, n);
-            printf("\nArreglo ordenado con Selection Sort:\n");
-            imprimirArreglo(arreglo, n);
+    int arreglo[MAX];
+    int opc; 
+    do {
+        printf("\n==== MENU ====\n");
+        printf("1. Llenar array\n");
+        printf("2. Mostrar array\n");
+        printf("3. Ordenar array (Selection Sort)\n");
+        printf("4. Ordenar array (Insertion Sort)\n");
+        printf("5. Salir\n");
+        printf("Ingrese una opcion: ");
+        scanf("%d", &opc);
+        switch (opc) {
+            case 1: 
+                llenarArray(arreglo, MAX);
             break;
-        case 2:
-            insertionSort(arreglo, n);
-            printf("\nArreglo ordenado con Insertion Sort:\n");
-            imprimirArreglo(arreglo, n);
+            case 2:
+                mostrarArray(arreglo, MAX);
             break;
-        default:
-            printf("Opcion invalida.\n");
-            return 1;
-    }
+            case 3:
+                selectionSort(arreglo, MAX);
+            break;
+            case 4:
+                insertionSort(arreglo, MAX);
+            break;
+        }
+    } while(opc != 5);
 
+    printf("Muchas Gracias\n");
     return 0;
 }
 
-void selectionSort(int arr[], int n) {
-    int i, j, min_idx, temp;
-
-    for (i = 0; i < n - 1; i++) {
-        min_idx = i;
-        for (j = i + 1; j < n; j++) {
-            if (arr[j] < arr[min_idx]) {
-                min_idx = j;
-            }
+void llenarArray(int vector[MAX], int n) {
+    for(int i = 0; i < n; i++) {
+        printf("Ingrese el valor de la posicion %d: ", i);
+        scanf("%d", &vector[i]);
+        while (vector[i] < 0) {
+             printf("Error, ingrese un valor positivo %d: ", i);
+             scanf("%d", &vector[i]);
         }
-        
-        temp = arr[min_idx];
-        arr[min_idx] = arr[i];
-        arr[i] = temp;
     }
 }
 
-
-void insertionSort(int arr[], int n) {
-    int i, key, j;
-
-    for (i = 1; i < n; i++) {
-        key = arr[i];
-        j = i - 1;
-
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
-        }
-        arr[j + 1] = key;
-    }
-}
-
-void imprimirArreglo(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+void mostrarArray(int vector[MAX], int n) {
+    for(int i = 0; i < n; i++) {
+        printf("%d  ", vector[i]);
     }
     printf("\n");
+}
+
+void selectionSort(int array[MAX], int n) { 
+    for (int i = 0; i < n - 1; i++) { 
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) { 
+            if (array[j] < array[minIndex]) { 
+                minIndex = j; 
+            } 
+        }
+        int temp = array[i]; 
+        array[i] = array[minIndex]; 
+        array[minIndex] = temp;
+
+        printf("\nIntercambio en paso %d:\n", i + 1);
+        mostrarArray(array, n);
+    } 
+} 
+
+void insertionSort(int arr[MAX], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];  
+            j--;
+        }
+        arr[j + 1] = key;
+
+        printf("\nPaso %d:\n", i);
+        mostrarArray(arr, n);
+    }
 }
